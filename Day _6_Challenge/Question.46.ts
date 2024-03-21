6ccafd248194e325350a7392a0fa6f459aa4bcfa
Ameer hamza <159821980+ameerhamza2524@users.noreply.github.com>
2024-03-21 11:24:21 +0500

    Add files via upload

---
+++ Day _6_Challenge/Question.46.ts
@@ -0,0 +1,15 @@
+let laptop ={
+    make: 'Hp',
+    model: 'Core_i5',
+    Systum_type: '64-bit Best opretor systum',
+    year: '2002',
+    Discribe: function() {
+        console.log(`his laptop is a 
+            make:${this.make}
+            nodel:${this.model} 
+            systum_type:${this.Systum_type} 
+            year:${this.year}`)
+    }
+
+};
+laptop.Discribe();
