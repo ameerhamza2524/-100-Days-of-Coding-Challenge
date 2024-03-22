c4355cc4cf0e96268284e619065f0546c8dd9183
Ameer hamza <159821980+ameerhamza2524@users.noreply.github.com>
2024-03-22 15:36:59 +0500

    Add files via upload

---
+++ Day 16/Qno.47.ts
@@ -0,0 +1,15 @@
+let Laptops = [{
+    make: 'HP', modal: "corei5", year: 2002
+
+},
+{make: 'Apple', model: 'macBook', year: 2024  },
+{make: 'Deal', model: 'corei7', year: 2021}
+];
+
+let [Laptop1, Laptop2, Laptop3 ] = Laptops;
+
+console.log(Laptop1)
+console.log(Laptop2)
+console.log(Laptop3)
+console.log(Laptops)
+
