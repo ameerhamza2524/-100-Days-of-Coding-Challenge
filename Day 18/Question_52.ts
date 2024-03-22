be06534faaa856c7d41727e726e82fdae8dbef61
Ameer hamza <159821980+ameerhamza2524@users.noreply.github.com>
2024-03-22 15:46:07 +0500

    Add files via upload

---
+++ Day 18/Question_52.ts
@@ -0,0 +1,13 @@
+// Sets up details about a smartphone
+let smartphone = {
+    make: "I phone",
+    model: "Iphone 11",
+    specs: {
+        storage: "128GB",
+        screenSize: "5.1 inches",
+        batteryLife: "8 hours"
+    }
+};
+
+// Shows what we've set up about the smartphone
+console.log(smartphone);
