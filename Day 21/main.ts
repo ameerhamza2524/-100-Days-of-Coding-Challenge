18c543e648927999b370871a1ce0e8077a8dd9ed
Ameer hamza <159821980+ameerhamza2524@users.noreply.github.com>
2024-03-22 15:49:37 +0500

    Add files via upload

---
+++ Day 21/main.ts
@@ -0,0 +1,10 @@
+// Making a list (enum) for different types of forots
+enum forotsType {
+    apple,
+    mango,
+    orenge
+}
+
+// Showing one type of forotstype from the list
+console.log(forotsType.mango); // It shows 0 because enums start counting from 0
+// Here, we're just checking what number the apple category got in our list.
