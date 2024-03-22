fee83a273436a6d97daa2f8e0d4e62058dff8636
Ameer hamza <159821980+ameerhamza2524@users.noreply.github.com>
2024-03-22 15:35:08 +0500

    Delete Day _6_Challenge directory

--- Day _6_Challenge/Qno.48.ts
+++
@@ -1,4 +0,0 @@
-let pricesSet1 = [30000, 10500, 11000];
-let pricesSet2 = [12000, 13000, 16020];
-let combinedPrices = [...pricesSet1, ...pricesSet2].sort((a, b) => a - b);
-console.log(combinedPrices);
